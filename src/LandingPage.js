16432a187bf8dfa36debd8200d6beb0bacb4985f
Kalama Kiefer <kalamakiefer@gmail.com>
2023-04-13 12:21:29 -1000

    feat: center landind page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,11 +1,12 @@
 import { Button } from "react-bootstrap";
 
-
 export default function LandingPage() {
-    return (
-        <div className="text-center">
-            <div className="h1 mb-5 mt-5">Welcome to Budget App</div>
-            <Button href="/budget" variant="outline-primary" >Start Budgeting</Button>
-        </div>
-    )
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center vh-100">
+      <div className="h1 mb-4">Welcome to Budget App</div>
+      <Button href="/budget" variant="outline-primary">
+        Start Budgeting
+      </Button>
+    </div>
+  );
 }
